9ad278c74df005e61893ce7df95224410c09e782
Anthony Ikeda <anthony.ikeda@gmail.com>
2023-02-08 14:14:51 -0800

    Added listener and snapshot repos

---
+++ src/main/java/com/example/pulsarproducerapp/ListenerComponent.java
@@ -0,0 +1,17 @@
+package com.example.pulsarproducerapp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.pulsar.annotation.PulsarListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ListenerComponent {
+
+    private final Logger log = LoggerFactory.getLogger(ListenerComponent.class);
+
+    @PulsarListener(subscriptionName = "spring-app", topics = {"persistent://public/default/alerts"})
+    void listen(String message) {
+        log.info("Message received {}", message);
+    }
+}
